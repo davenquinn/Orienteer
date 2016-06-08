8e39a3b109660484e85b66e5c0d1a62bb014b5ea
Daven Quinn <dev@davenquinn.com>
2016-06-08 03:46:57 -0700

    Added utility function to parse PostGIS geometry to Shapely

---
+++ elevation/util/sql.py
@@ -0,0 +1,9 @@
+from shapely.wkb import loads
+from binascii import unhexlify
+
+def parse_geometry(wkb):
+    """
+    Parse geometry from PostGIS into a shapely
+    geometry.
+    """
+    return loads(unhexlify(wkb.encode()))
