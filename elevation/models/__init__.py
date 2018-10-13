357cbf401bb065f82703126957dc7b2ca0e59d5c
Daven Quinn <dev@davenquinn.com>
2018-10-13 01:16:12 -0700

    Make sure we load flask app correctly

--- elevation/models/__init__.py
+++ elevation/models/__init__.py
@@ -4,3 +4,5 @@ from .feature import DatasetFeature
 from .attitude import Attitude, AttitudeGroup, Tag
 from ..database import db
 
+from pg_projector import init_models
+Projection = init_models(db)
