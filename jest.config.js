7e722a702febae71b82824046f927c4a219603b9
Daven Quinn <dev@davenquinn.com>
2022-01-25 15:14:18 -0600

    Test actually runs successfully

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const esModules = ["d3-array"].join("|");
+
 module.exports = {
   // globals: {
   //   "ts-jest": {
@@ -9,10 +11,25 @@ module.exports = {
   rootDir: "frontend",
   moduleFileExtensions: ["ts", "tsx", "js"],
   testMatch: ["**/*.test.+(ts|tsx|js)"],
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
-  },
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/tests/mocks.ts"],
   //setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  transformIgnorePatterns: ["(?!d3|d3-array)"],
+  //transformIgnorePatterns: [],
+  //testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  //testEnvironment: "jsdom",
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+        https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    "^.+\\.(js|jsx|ts|tsx)$": require.resolve("babel-jest"),
+  },
+  moduleNameMapper: {
+    "\\.(css|less|styl)$": "identity-obj-proxy",
+    "^app(.*)$": "<rootDir>/src/$1",
+    "@macrostrat/map-components": "@macrostrat/map-components/src",
+  },
+
+  transformIgnorePatterns: [
+    //`node_modules/(?!(d3-array|d3)/)`,
+    "^.+\\.module\\.(css|sass|scss|styl)$",
+  ],
 };
