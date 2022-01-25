9feaf545bc42898a74dcb794c2dea06e436886e0
Daven Quinn <dev@davenquinn.com>
2022-01-24 20:51:34 -0600

    Compiles successfully using webpack v5

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  presets: ["@parcel/babel-preset-env"],
-  plugins: ["@parcel/babel-plugin-transform-runtime"],
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [],
 };
